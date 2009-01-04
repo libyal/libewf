92e0f3934b4263396c078d769ea7a27346f8b9fd
Joachim Metz <joachim.metz@gmail.com>
2009-01-04 21:11:54 +0100

    Changes of version: 20070714

--- include/libewf/libewf_definitions.h
+++ include/libewf/libewf_definitions.h
@@ -37,9 +37,9 @@
 /* The libewf version
  */
 #if 0
-#define LIBEWF_VERSION				L"20070713"
+#define LIBEWF_VERSION				L"20070714"
 #else
-#define LIBEWF_VERSION				"20070713"
+#define LIBEWF_VERSION				"20070714"
 #endif
 
 /* The libewf file access
