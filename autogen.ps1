1a8b1f1210afcfec93aac403ecd9a0ebc6a14025
Joachim Metz <joachim.metz@gmail.com>
2015-01-25 18:01:11 +0100

    Worked on AppVeyor test configuration.

--- autogen.ps1
+++ autogen.ps1
@@ -5,10 +5,8 @@
 $Library = Get-Content -Path configure.ac | select -skip 3 -first 1 | % { $_ -Replace " \[","" } | % { $_ -Replace "\],","" }
 $Version = Get-Content -Path configure.ac | select -skip 4 -first 1 | % { $_ -Replace " \[","" } | % { $_ -Replace "\],","" }
 
-Get-Content -Path include\${Library}.h.in > include\${Library}.h
-Get-Content -Path include\${Library}.h | % { $_ -Replace "@HAVE_V1_API@","0" } > include\${Library}.h
-Get-Content -Path include\${Library}\definitions.h.in | % { $_ -Replace "@VERSION@",${Version} } > include\${Library}\definitions.h
-Get-Content -Path include\${Library}\definitions.h | % { $_ -Replace "@HAVE_V1_API@","0" } > include\${Library}\definitions.h
+Get-Content -Path include\${Library}.h.in | % { $_ -Replace "@HAVE_V1_API@","0" } > include\${Library}.h
+Get-Content -Path include\${Library}\definitions.h.in | % { $_ -Replace "@VERSION@",${Version} } | % { $_ -Replace "@HAVE_V1_API@","0" } > include\${Library}\definitions.h
 Get-Content -Path include\${Library}\features.h.in | % { $_ -Replace "@[A-Z0-9_]*@","0" } > include\${Library}\features.h
 Get-Content -Path include\${Library}\types.h.in | % { $_ -Replace "@[A-Z0-9_]*@","0" } > include\${Library}\types.h
 Get-Content -Path common\types.h.in | % { $_ -Replace "@PACKAGE@",${Library} } > common\types.h
