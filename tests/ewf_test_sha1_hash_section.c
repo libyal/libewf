f40d3f7b403f9f800badcda476cd0635c3af991f
Joachim Metz <joachim.metz@gmail.com>
2018-01-25 07:47:29 +0100

    Worked on tests

---
+++ tests/ewf_test_sha1_hash_section.c
@@ -0,0 +1,231 @@
+/*
+ * Library SHA1 hash section functions test program
+ *
+ * Copyright (C) 2006-2018, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "ewf_test_libcerror.h"
+#include "ewf_test_libewf.h"
+#include "ewf_test_macros.h"
+#include "ewf_test_unused.h"
+
+#include "../libewf/libewf_sha1_hash_section.h"
+#include "../libewf/libewf_hash_sections.h"
+#include "../libewf/libewf_section_descriptor.h"
+
+uint8_t ewf_test_sha1_hash_section_data1[ 32 ] = {
+	0x5c, 0x17, 0x4b, 0xf2, 0x89, 0xe5, 0xa0, 0xea, 0xdd, 0xef, 0x71, 0x9c, 0x59, 0x59, 0xaf, 0xfc,
+	0x1d, 0xff, 0x8d, 0x23, 0xab, 0x0b, 0x60, 0x7a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+#if defined( __GNUC__ ) && !defined( LIBEWF_DLL_IMPORT )
+
+/* Tests the libewf_sha1_hash_section_read_data function
+ * Returns 1 if successful or 0 if not
+ */
+int ewf_test_sha1_hash_section_read_data(
+     void )
+{
+	libcerror_error_t *error              = NULL;
+	libewf_hash_sections_t *hash_sections = NULL;
+	int result                            = 0;
+
+	/* Initialize test
+	 */
+	result = libewf_hash_sections_initialize(
+	          &hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EWF_TEST_ASSERT_IS_NOT_NULL(
+	 "hash_sections",
+	 hash_sections );
+
+	EWF_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test regular cases
+	 */
+	result = libewf_sha1_hash_section_read_data(
+	          ewf_test_sha1_hash_section_data1,
+	          32,
+	          hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EWF_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libewf_sha1_hash_section_read_data(
+	          NULL,
+	          32,
+	          hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EWF_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libewf_sha1_hash_section_read_data(
+	          ewf_test_sha1_hash_section_data1,
+	          (size_t) SSIZE_MAX + 1,
+	          hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EWF_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libewf_sha1_hash_section_read_data(
+	          ewf_test_sha1_hash_section_data1,
+	          0,
+	          hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EWF_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = libewf_sha1_hash_section_read_data(
+	          ewf_test_sha1_hash_section_data1,
+	          32,
+	          NULL,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EWF_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libewf_hash_sections_free(
+	          &hash_sections,
+	          &error );
+
+	EWF_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EWF_TEST_ASSERT_IS_NULL(
+	 "hash_sections",
+	 hash_sections );
+
+	EWF_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	if( hash_sections != NULL )
+	{
+		libewf_hash_sections_free(
+		 &hash_sections,
+		 NULL );
+	}
+	return( 0 );
+}
+
+#endif /* defined( __GNUC__ ) && !defined( LIBEWF_DLL_IMPORT ) */
+
+/* The main program
+ */
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc EWF_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] EWF_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc EWF_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] EWF_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	EWF_TEST_UNREFERENCED_PARAMETER( argc )
+	EWF_TEST_UNREFERENCED_PARAMETER( argv )
+
+#if defined( __GNUC__ ) && !defined( LIBEWF_DLL_IMPORT )
+
+	EWF_TEST_RUN(
+	 "libewf_sha1_hash_section_read_data",
+	 ewf_test_sha1_hash_section_read_data );
+
+#endif /* defined( __GNUC__ ) && !defined( LIBEWF_DLL_IMPORT ) */
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
